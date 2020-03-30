4cfedccc7977ef47aa9372ecc96c86951eb70448
alexandra-v-n <alexandra.neculae97@gmail.com>
2020-03-30 13:18:57 +0300

    Lab 7

---
+++ app/src/main/java/com/example/laborator7/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.laborator7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    TextToSpeech t1;
+    TextView txt1;
+    Button b1;
+    private static final int SPEECH_REQUEST_CODE = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txt1=findViewById(R.id.textView);
+        b1=findViewById(R.id.button2);
+
+
+
+        t1=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    t1.setLanguage(Locale.UK);
+                }
+            }
+        });
+
+
+    }
+
+    public void onPause(){
+        if(t1 !=null){
+            t1.stop();
+            t1.shutdown();
+        }
+        super.onPause();
+    }
+
+    // Create an intent that can start the Speech Recognizer activity
+    public void  displaySpeechRecognizer(View v) {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+// Start the activity, the intent will be populated with the speech text
+        startActivityForResult(intent, SPEECH_REQUEST_CODE);
+    }
+
+    public void textToSpeech(View v) {
+        String toSpeak = txt1.getText().toString();
+        Log.e("TEST",toSpeak);
+        Toast.makeText(getApplicationContext(), toSpeak,Toast.LENGTH_SHORT).show();
+        t1.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        if (requestCode == SPEECH_REQUEST_CODE && resultCode == RESULT_OK) {
+            List<String> results = data.getStringArrayListExtra(
+                    RecognizerIntent.EXTRA_RESULTS);
+            String spokenText = results.get(0);
+            txt1.setText(spokenText);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }}
+
